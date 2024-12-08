4158cfa1838254ea840d084a2fc4f273abcdd2ee
NikoM017 <115002298+NikoM017@users.noreply.github.com>
2024-12-08 12:13:11 +0100

    Add files via upload

--- Weather App Thing/script.js
+++ Weather App Thing/script.js
@@ -1,4 +1,4 @@
-const apikey = ySEexcyT625xN4x0DoZ5OQ7ygtNtoqEn;
+const apikey = "ySEexcyT625xN4x0DoZ5OQ7ygtNtoqEn";
 function getWeather() {
   const searchBar = document.getElementById("searchInput");
   if (!searchBar) {
@@ -8,7 +8,7 @@ function getWeather() {
   const options = { method: "GET", headers: { accept: "application/json" } };
 
   fetch(
-    `https://api.tomorrow.io/v4/weather/realtime?location=${searchBar}&apikey=${apikey}`,
+    `https://api.tomorrow.io/v4/weather/realtime?location=${encodeURIComponent(searchBar.value)}&apikey=${apikey}`,
     options
   )
     .then((res) => res.json())
@@ -18,15 +18,19 @@ function getWeather() {
         return;
       }
       const stuff = res.data.values;
-      document.getElementById('searchBar').innerText.res.searchBar.name
+      document.getElementById('searchBar').innerText = res.location.name;
       document.getElementById('temperature').innerText = stuff.temperature;
-      document.getElementById('humidity').innerText = stuff.temperature;
-      document.getElementById('windSpeed').innerText = stuff.temperature;
-      document.getElementById('uvIndex').innerText = stuff.temperature;
+      document.getElementById('humidity').innerText = stuff.humidity;
+      document.getElementById('windSpeed').innerText = stuff.windSpeed;
+      document.getElementById('uvIndex').innerText = stuff.uvIndex;
 
       document.getElementById('data1').style.display='block';
 
     })
 
-    .catch((err) => console.error(err));
-}
+    .catch((err) => {
+      console.error("error fetching weather data", err);
+      alert("Failed to fetch weather data. Please try again.");
+    });
+  }
+  
